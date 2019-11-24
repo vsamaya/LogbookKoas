33e3321b57d251be80663857b974bcfb4343b5ee
Samaya <val.samaya82@gmail.com>
2019-11-24 14:04:25 +0700

    Desain menu untu mahasiswa dan dosen

--- app/src/main/java/com/example/logbookkoas/MainActivity.java
+++ app/src/main/java/com/example/logbookkoas/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.logbookkoas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnDosen, btnMahasiswa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDosen = findViewById(R.id.btn_dosen);
+        btnMahasiswa = findViewById(R.id.btn_mhsw);
+
+        btnDosen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DosenActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnMahasiswa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MahasiswaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
