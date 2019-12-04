22703e4442a68696e46bb1ab7c9ffec165c6ca8f
E1Creation <kurniawanplusayahibu@gmail.com>
2019-12-04 23:41:27 +0700

    edit profil beta

--- app/src/main/java/com/example/logbookkoas/DosenActivity.java
+++ app/src/main/java/com/example/logbookkoas/DosenActivity.java
@@ -8,28 +8,26 @@ import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class DosenActivity extends AppCompatActivity {
 Button btn_generatecode,btn_generateqr,btn_dftrkeg,btn_rekapkeg;
-ImageView profile;
-TextView username;
-    private SessionHandler session;
+ImageView iconprofil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dosen);
-        session = new SessionHandler(getApplicationContext());
-        User user = session.getUserDetails();
         btn_generatecode=findViewById(R.id.btn_generatecode);
         btn_generateqr=findViewById(R.id.btn_generateqr);
         btn_dftrkeg=findViewById(R.id.btn_dftrkeg);
         btn_rekapkeg=findViewById(R.id.btn_rekapkeg);
-        profile = findViewById(R.id.img_profile_dsn);
-        username = findViewById(R.id.usernamedosen);
-        username.setText(user.getUsername());
-
-
+        iconprofil=findViewById(R.id.iconprofil);
+        iconprofil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent icon=new Intent(DosenActivity.this,profilds.class);
+                startActivity(icon);
+            }
+        });
         btn_generatecode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,22 +41,6 @@ TextView username;
             public void onClick(View view) {
                 Intent b = new Intent(DosenActivity.this, MainQR.class);
                 startActivity(b);
-            }
-        });
-
-        btn_dftrkeg.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent c = new Intent(DosenActivity.this, DaftarKegiatan.class);
-                startActivity(c);
-            }
-        });
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DosenActivity.this, profilds.class);
-                startActivity(i);
-                finish();
             }
         });
 
